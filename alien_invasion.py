622ec557a8b282f8c292d9637e6e4479372b271f
Matt Labriola <matt.labriola@gmail.com>
2026-01-07 17:14:54 -0800

    getting started on main code

---
+++ alien_invasion.py
@@ -0,0 +1,12 @@
+"""
+alien_invasion game
+Python Crash Course chapters 12 - 14
+Creating a 2D game using python and pygame
+"""
+# import packages
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """"""
