42f286baf8ed32a1d555046156beccb75abee44b
Matt L. <matt.labriola@gmail.com>
2026-02-03 15:08:05 -0500

    Update alien.py added edge detection and reversal

--- alien.py
+++ alien.py
@@ -29,5 +29,11 @@ class Alien(Sprite):
 
     def update(self):
         """ Move alien to the right """
-        self.x += self.settings.alien_speed
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
+
+    def check_edges(self):
+        """ Return True if alien is at edge, False if in center of screen """
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+        
